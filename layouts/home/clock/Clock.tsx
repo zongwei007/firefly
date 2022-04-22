7a3b057b3ff761fbc2f23d66eaf3f9081166341e
zongwei007 <zongwei007@gmail.com>
2022-04-22 20:56:50 +0800

    feat: 基于天气的日出/日落时间计算时钟欢迎信息

--- layouts/home/clock/Clock.tsx
+++ layouts/home/clock/Clock.tsx
@@ -1,27 +1,37 @@
 import type { FC } from 'react';
-import { useState, useRef } from 'react';
+import { useRef, useState } from 'react';
 import type { Locale } from 'date-fns';
-import { format as formatDateTime, startOfDay, subDays, setHours, differenceInHours, isBefore } from 'date-fns';
+import { differenceInMinutes, format as formatDateTime, isAfter, isBefore, parse as parseDateTime } from 'date-fns';
 import classNames from 'classnames';
 import { useIsomorphicLayoutEffect } from 'react-use';
 import { zhCN } from 'date-fns/locale';
 import styles from './style.module.css';
-import { useSettings } from 'hooks';
+import { useSettings, useWeather } from 'hooks';
 
 type ClockProps = { className?: string; defaultValue: number; locale?: Locale };
 
 const Clock: FC<ClockProps> = ({ className, defaultValue, locale = zhCN }) => {
   const { data: config } = useSettings();
+  const { data: weather } = useWeather();
   const timestamp = useTimestamp(defaultValue);
-  let titles = config?.ui.clock.welcome.split(';') || [];
-  //早上从 6 点开始
-  let start = setHours(startOfDay(timestamp), 6);
-  if (isBefore(timestamp, start)) {
-    start = subDays(start, 1);
+
+  let welcome = '';
+  const titles = config?.ui.clock.welcome.split(';') || [];
+
+  if (weather) {
+    const { sunrise, sunset } = weather.today;
+    const sunriseTime = parseDateTime(sunrise, 'HH:mm', timestamp);
+    const sunsetTime = parseDateTime(sunset, 'HH:mm', timestamp);
+
+    if (isAfter(timestamp, sunriseTime) && isBefore(timestamp, sunsetTime)) {
+      const duration = differenceInMinutes(sunsetTime, sunriseTime, { roundingMethod: 'floor' });
+      const diff = differenceInMinutes(timestamp, sunriseTime, { roundingMethod: 'floor' });
+      const pos = Math.floor((diff / duration) * titles.length);
+      welcome = titles[pos];
+    } else {
+      welcome = titles[titles.length - 1];
+    }
   }
-  const diff = differenceInHours(timestamp, start, { roundingMethod: 'floor' });
-  const pos = Math.floor(diff / 24 * titles.length);
-  titles = [titles[pos]];
 
   return (
     <div className={classNames(className, styles.clock)}>
@@ -29,7 +39,7 @@ const Clock: FC<ClockProps> = ({ className, defaultValue, locale = zhCN }) => {
         <span>{formatDateTime(timestamp, 'PPPP', { locale })}</span>
         <span>{formatDateTime(timestamp, 'HH:mm:ss')}</span>
       </p>
-      <h1>{titles}</h1>
+      <h1>{welcome}</h1>
     </div>
   );
 };
